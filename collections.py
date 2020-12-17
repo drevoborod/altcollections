08f6b916df43f480629b86af8d65dff3d86c3f14
Aleksey <drevoborod@gmail.com>
2020-12-17 21:18:34 +0300

    Renamed to ExtendedDict and added new functionality

--- collections.py
+++ collections.py
@@ -1,12 +1,60 @@
-class TupleDict(dict):
+class ExtendedDict(dict):
+    """
+    Расширение базового словаря. Новые возможности:
+    - возможность доступа и добавления ключей через точечную нотацию.
+        То есть можно добавить как ключ (d['key'] = 1), а можно - как атрибут:
+        d.key1 = 2. Доступ после этого будет возможен как через ключ,
+        так и через атрибут: d['key'] или d.key. Для тех, кому надоело писать
+        скобочки и кавычки. При этом выгодное отличие от данных в виде объектов -
+        сохранение плюшек словаря: методы keys и items, итератор, update и т.д.
+    - поддержка операции умножения. При этом возвращается новый экземпляр
+        ExtendedDict, у которого все значения умножены на предоставленное число.
+        Удобно, когда нужно увеличить все значения в словаре на что-то одно.
+        Прочие арифметические операции пока не поддерживаются за ненадобностью,
+        но, может будет добавлено в будущем.
+    - новый метод crop: возвращает НОВЫЙ экземпляр ExtendedDict,
+        из которого выкинут соответствующий ключ.
+        Нужно для однострочников. Метод оригинального словаря pop меняет
+        исходный словарь, поэтому требуется создавать экземпляр словаря,
+        потом делать в нём pop отдельной строкой, а потом уже использовать
+        получившийся результат. А в ExtendedDict можно сделать так:
+        `return cls.date_time(**ExtendedDict(locals()).crop('cls')`
+        Здесь мы из словаря locals выкидываем ключ 'cls' и сразу же
+        к получившемуся результату применяем date_time(). Это удобно,
+        потому что не нужно заводить отдельную переменную вроде
+        "params = locals()" - у нас автоматически создаётся копия,
+        из которой при применении метода crop опять же возвращается копия.
+        В случае с pop нам бы вернулось значение удаляемого ключа, что далеко
+        не всегда нужно.
+    - новый метод add: расширение метода update, которое позволит не изменять
+        исходный словарь, а возвращать новый экземпляр с изменениями.
+    """
+
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
+        # All inner dictionaries will be recursively converted to ExtendedDict.
+        for key, value in self.items():
+            self[key] = value
 
-    def __getitem__(self, item):
-        if type(item) in (int, slice):
-            return tuple(self.values())[item]
-        else:
-            return dict.__getitem__(self, item)
+    def _check_type(self, value):
+        if hasattr(value, 'keys'):
+            return self._convert_dict(value)
+        elif isinstance(value, (list, tuple)):
+            return self._convert_iterable(value)
+        return value
+
+    def _convert_dict(self, dictionary):
+        result = self.__class__(dictionary)
+        for key, value in result.items():
+            result[key] = self._check_type(value)
+        return result
+
+    def _convert_iterable(self, iterable):
+        new = [self._check_type(item) for item in iterable]
+        return tuple(new) if isinstance(iterable, tuple) else new
+
+    def __iter__(self):
+        return iter(self.keys())
 
     def __getattr__(self, item):
         if item in self.keys():
@@ -18,19 +66,82 @@ class TupleDict(dict):
         self[key] = value
 
     def __setitem__(self, key, value):
+        dict.__setitem__(self, key, self._check_type(value))
+
+    def __mul__(self, other):
+        """
+        Returns new instance of ExtendedDict instantiated from 'self' where all
+        values of root keys are multiplied on value of 'other' (not recursive).
+        """
+        new = self.__class__(self)
+        for key, value in new.items():
+            try:
+                new[key] = value * other
+            except TypeError:
+                raise TypeError(f"can't multiply value of key '{key}' "
+                                f"by non-int of type '{type(other).__name__}'")
+        return new
+
+    def crop(self, *keys):
+        """Deletes provided keys from the dictionary and returns new ExtendedDict."""
+        new = self.__class__(self)
+        for key in keys:
+            new.pop(key, None)
+        return new
+
+    def add(self, __m=None, /, **kwargs):
+        """Adds provided dictionary to current and returns new instance of ExtendedDict."""
+        new = self.__class__(self)
+        new.update(__m, **kwargs)
+        return new
+
+    def update(self, __m=None, /, **kwargs) -> None:
+        if __m:
+            dict.update(self, self._check_type(__m), **self._convert_dict(kwargs))
+        else:
+            dict.update(self, **self._convert_dict(kwargs))
+
+    def copy(self):
+        return self.__class__(dict.copy(self))
+
+
+class TupleDict(ExtendedDict):
+    """
+    - добавлена возможность доступа по срезу и индексу непосредственно к значениям
+        (их порядок сохраняется), то есть:
+            d['key'] = 1
+            d['key1'] = 2
+        d[0] вернёт 1, который мы выше добавили
+        d[:] вернёт кортеж (1, 2).
+        У этого есть побочный эффект:
+        невозможно обратиться к ключам, имеющим тип int, поскольку такие обращения
+        автоматически считаются обращением по индексу. Но кто в реальной жизни
+        использует словари с int в качестве ключей? Встречается редко.
+        И, если встретится, всегда можно использовать обычный словарь :)
+    - новый метод crop_astuple: возвращает кортеж из значений словаря,
+        из которого убрано значение, соответствующее ключу словаря, переданному методу.
+    """
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+    def __getitem__(self, item):
+        if type(item) in (int, slice):
+            return tuple(self.values())[item]
+        else:
+            return dict.__getitem__(self, item)
+
+    def __setitem__(self, key, value):
         if isinstance(key, int):
             raise TypeError(f'Integers cannot be added as '
                             f'{self.__class__.__name__} keys')
         else:
-            dict.__setitem__(self, key, value)
+            self.__class__.__bases__[0].__setitem__(self, key, value)
 
-    def crop(self, key):
-        """Deletes provided key from the dictionary and returns dictionary."""
-        del self[key]
-        return self
-
-    def crop_astuple(self, key):
-        """Deletes provided key from the dictionary and
-        returns tuple of values."""
-        del self[key]
-        return self[:]
+    def crop_astuple(self, *keys):
+        """Deletes provided keys from the dictionary and
+        returns tuple of values of new TupleDict."""
+        new = self.__class__(self)
+        for key in keys:
+            new.pop(key, None)
+        return new[:]
