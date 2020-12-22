8182a050601a0b2c6c7570ffadf29f126e628ef3
Aleksey <drevoborod@gmail.com>
2020-12-22 19:09:31 +0300

    Create compare.py

---
+++ compare.py
@@ -0,0 +1,12 @@
+from deepdiff import DeepDiff
+
+from .collections import ExtendedDict, TupleDict
+
+
+class DictDiff(DeepDiff):
+    """Allows to compare two instances of custom dictionaries and basic dict"""
+    def __init__(self, *args, **kwargs):
+        if 'ignore_type_in_groups' not in kwargs:
+            kwargs['ignore_type_in_groups'] = [
+                (dict, ExtendedDict), (dict, TupleDict), (ExtendedDict, TupleDict)]
+        super().__init__(*args, **kwargs)
